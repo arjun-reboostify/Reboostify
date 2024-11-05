a67740141354e4acb902829394f9475cb2f92f80
arjun-reboostify <reboostify@gmail.com>
2024-11-05 21:43:15 +0530

    f

--- src/hooks/useDataContext.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react'
-import { DataContext, DataContextInterface } from '../contexts/DataContext'
-
-export const useDataContext = () => {
-  return useContext(DataContext) as DataContextInterface
-}
--- src/hooks/useErrorPromptContext.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react'
-import { ErrorPromptContext, ErrorPromptContextInterface } from '../contexts/ErrorPromptContext'
-
-export const useErrorPromptContext = () => {
-  return useContext(ErrorPromptContext) as ErrorPromptContextInterface
-}
